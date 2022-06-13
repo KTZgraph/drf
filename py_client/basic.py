6d911b15f7af91f73994de6dca9e36416bdfa9a8
pawlaczykdominika <pawlaczyk@protonmail.com>
2022-06-13 21:32:16 +0200

    [37:07] Create your first API View

--- py_client/basic.py
+++ py_client/basic.py
@@ -59,7 +59,8 @@ endpoint = "http://httpbin.org/anything" # zwróci odpwoiedź formatted data tha
 # print(get_response.status_code)
 
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 get_response = requests.get(endpoint, json={'query': 'Hello World'}) # HTTP  Request
 print(get_response.text) #print raw response
 print(get_response.status_code)
+print(get_response.json()['message'])
